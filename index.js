68751cb4a2f2d88bf400b481ed0231be7c0a0ce3
Dilara Katuk <dilarakatuk@gmail.com>
2022-04-20 23:38:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+   console.log(customerName);
+};
+// upperCaseCustomerName();
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = "not bob";
+   console.log(bestCustomer);
+}
+// setBestCustomer();
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    console.log(bestCustomer);
+}
+// overwriteBestCustomer();
+
+const leastFavoriteCustomer = "john"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "jane";
+    console.log(leastFavoriteCustomer);
+}
+// changeLeastFavoriteCustomer();
+
+
+
+
+
+
+
